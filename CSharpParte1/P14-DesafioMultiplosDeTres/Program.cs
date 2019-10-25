6856d3d907b8de8dbdec73ee1a111581a1f032b8
Diego Luques <luquesdiego@outlook.com>
2019-10-25 14:59:12 -0400

    Desafio Múltiplos de 3

---
+++ CSharpParte1/P14-DesafioMultiplosDeTres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14_DesafioMultiplosDeTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto P14 - Multíplos de 3");
+
+            for (int numero = 1; numero < 100; numero++)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

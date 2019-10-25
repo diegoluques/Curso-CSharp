f02d22b69bb456890ab8f31927efe6fa5d915c6a
Diego Luques <luquesdiego@outlook.com>
2019-10-25 13:48:03 -0400

    While e For - 1

---
+++ CSharpParte1/P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto P10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            int mes = 1;
+
+            while (mes <= 12)
+            {
+                // 0.36% = 0.0036
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + mes + " mês, você terá R$ " + valorInvestido);
+                mes++;
+            }
+
+            Console.WriteLine("Fim da execução");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpParte1/P11-CalculaPoupanca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução do projeto P11 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            for (int mes = 1; mes <= 12; mes ++)
+            {
+                valorInvestido *= 1.0030;
+                Console.WriteLine("Após " + mes + ", você terá R$ " + valorInvestido);
+            }
+
+            Console.WriteLine("Fim da execução");
+            Console.ReadLine();
+        }
+    }
+}

22b3a8ab8230740661a65bf5734a0b0d79a74a48
Diego Luques <luquesdiego@outlook.com>
2019-10-25 13:05:47 -0400

    Condicionais

---
+++ CSharpParte1/7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int qtdPessoas = 2;
+            if (idadeJoao > 18)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade. Pode liberar a entrada");
+            }
+            else
+            {
+                if (qtdPessoas >= 2)
+                {
+                    Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado. Pode entrar");
+                }
+                else
+                {
+                    Console.WriteLine("João não possui mais de 18 anos e não está acompanhado. Não pode entrar");
+                }
+                Console.WriteLine("João tem a idade menor que 18 anos. Não pode entrar");
+            }
+
+            Console.WriteLine("Fim da execução");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpParte1/8-Condicionas2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int qtdPessoas = 2;
+            bool acompanhado = qtdPessoas >= 2;
+            string mensagemAdiconal;
+
+            if (acompanhado == true)
+            {
+                mensagemAdiconal = "João está acompanhado!";
+            }
+            else
+            {
+                mensagemAdiconal = "João bão está acaompanhado!";
+            }
+
+            if (idadeJoao > 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode liberar a entrada");
+                Console.WriteLine(mensagemAdiconal);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+                Console.WriteLine(mensagemAdiconal);
+            }
+
+            Console.WriteLine("Fim da execução");
+            Console.ReadLine();
+        }
+    }
+}

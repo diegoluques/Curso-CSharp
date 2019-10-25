949f9ca26e71e2808d03bd2bfc5f02675ff34e56
Diego Luques <luquesdiego@outlook.com>
2019-10-25 14:53:17 -0400

    For encadeado

---
+++ CSharpParte1/P13-ForEncadeado/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto P13 - For Encadeado");
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+            //********
+
+            //Escrevendo os * com o break
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            //Uma forma diferente de fazer desenhos com *
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

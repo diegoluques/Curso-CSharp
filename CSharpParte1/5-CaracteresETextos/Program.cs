b141f78061f2250c7f94863b090ce80510e12b29
Diego Luques <luquesdiego@outlook.com>
2019-10-25 12:16:49 -0400

    Caracteres, atribuições de valiáveis

---
+++ CSharpParte1/5-CaracteresETextos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+            //Character
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia ";
+            string cursosProgramacao =
+@"- .NET
+- JAVA 
+- JAVASCRIPT";
+            Console.WriteLine(titulo + 2020);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("Fim da execução");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpParte1/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Início do projeto 6 - Atribuições de Variáveis.");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.WriteLine("Fim da execulção");
+            Console.ReadLine();
+        }
+    }
+}

750362895165f31d85a340774ad5cdc3664ba1d9
Diego Luques <luquesdiego@outlook.com>
2019-10-28 09:06:21 -0400

    Inserindo observações

--- CSharpParte1/P15-DesafioFatorial/Program.cs
+++ CSharpParte1/P15-DesafioFatorial/Program.cs
@@ -12,14 +12,25 @@ namespace P15_DesafioFatorial
         {
             Console.WriteLine("Executando o projeto P15 - Fatorial");
 
-            int fatorial = 1;
+            int fatorial1 = 1;
             for (int i = 1; i < 11; i++)
             {
-                fatorial *= i;
-                Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+                fatorial1 *= i;
+                Console.WriteLine("Fatorial de " + i + " = " + fatorial1);
             }
 
-            Console.WriteLine(fatorial);
+            Console.WriteLine("");
+
+            Console.WriteLine("Utilizando while");
+            int fatorial = 1;
+            int contador = 1;
+            while (contador < 11)
+            {
+                fatorial *= contador;
+                Console.WriteLine("Fatorial de " + contador + " = " + fatorial);
+                contador++;
+            }
+
             Console.ReadLine();
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Curso-CSharp
-Curso de CSharp da Alura
+Curso de CSharp Parte 1 da Alura
+
+# Este curso abordou os seguintes tópicos:
+- História e ecossistema da linguagem
+- Nosso primeiro programa e o Visual Studio
+- Variáveis numéricas
+- Variáveis de texto
+- Controle de fluxo com IF
+- Controle de fluxo com laços de repetição

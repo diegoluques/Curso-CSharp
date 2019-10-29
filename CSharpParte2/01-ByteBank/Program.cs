a9d5a7cb36f1e874e530543fe8cb0e58862af52b
Diego Luques <luquesdiego@outlook.com>
2019-10-29 13:18:13 -0400

    Usando nossa classe

--- CSharpParte2/01-ByteBank/Program.cs
+++ CSharpParte2/01-ByteBank/Program.cs
@@ -10,7 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            
+            contaDaGabriela.tirular = "Gabriela";
+            contaDaGabriela.agencia = 0123;
+            contaDaGabriela.numero = 028821;
+            contaDaGabriela.saldo = 1200.00;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.tirular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Conta: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: R$ " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200.00;
+            Console.WriteLine("Saldo Novo: R$ " + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }

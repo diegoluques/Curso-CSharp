c1adc351b2aeb40a9dfb9b547534b1d38b01802c
Diego Luques <luquesdiego@outlook.com>
2019-10-30 13:27:15 -0400

    Nossa primeira funcao

---
+++ CSharpParte2/04-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.tirular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}

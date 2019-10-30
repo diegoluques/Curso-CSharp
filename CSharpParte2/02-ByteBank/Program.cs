13d78c6330ad0fd1f1092134d945561daf8a965a
Diego Luques <luquesdiego@outlook.com>
2019-10-30 12:40:17 -0400

    Valores padrões

---
+++ CSharpParte2/02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.tirular = "Gabriela";
+            conta.saldo = 2000.00;
+
+            Console.WriteLine(conta.tirular);
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}

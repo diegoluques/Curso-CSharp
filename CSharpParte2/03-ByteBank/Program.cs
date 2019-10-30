ebc1e36a176de5ba9bed3cef8a20877adc0e2ef1
Diego Luques <luquesdiego@outlook.com>
2019-10-30 12:57:23 -0400

    Tipos de referência e tipos de valor

---
+++ CSharpParte2/03-ByteBank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.tirular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 255561;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.tirular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 255561;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 25;
+            int idadeNova = 25;
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeNova));
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}

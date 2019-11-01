8bf77c3021b9546b9ac5431304cba71f64f41c17
Diego Luques <luquesdiego@outlook.com>
2019-11-01 12:56:25 -0400

    Produtividade com Get e Set

--- CSharpParte2/06-ByteBank/ContaCorrente.cs
+++ CSharpParte2/06-ByteBank/ContaCorrente.cs
@@ -4,8 +4,9 @@ namespace _06_ByteBank
     public class ContaCorrente
     {
         public Cliente Titular { get; set; } 
-        public int agencia;
-        public int numero;
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
         private double _saldo = 100.00;
 
         public double Saldo
--- CSharpParte2/06-ByteBank/Program.cs
+++ CSharpParte2/06-ByteBank/Program.cs
@@ -13,14 +13,14 @@ namespace _06_ByteBank
             ContaCorrente conta = new ContaCorrente();
             Cliente cliente = new Cliente();
 
-            cliente.nome = "Guilherme";
-            cliente.cpf = "434.564.897-20";
-            cliente.profissao = "Desenvolvedor";
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434.564.897-20";
+            cliente.Profissao = "Desenvolvedor";
 
             conta.Saldo = (-10);
             conta.Titular = cliente;
 
-            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Titular.Nome );
             Console.WriteLine(conta.Saldo);
 
             Console.ReadLine();

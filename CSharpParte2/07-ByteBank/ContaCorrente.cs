91c195b72ee4cc2eb2fbdc52e4e967805620bb9d
Diego Luques <luquesdiego@outlook.com>
2019-11-01 13:11:48 -0400

    Construtores

---
+++ CSharpParte2/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,66 @@
+﻿
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100.00;
+
+        public ContaCorrente (int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return this._saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                this._saldo = value;
+            }
+        }
+
+        //Quando retorna um valor, comum chamar de função, mas pode ser chamado de método também
+        //bool retorna true ou false
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            return true;
+        }
+
+        //Quando não retorna um valor, chama-se de método
+        //void não retorna nenhum valor
+        public void Depositar(double valorDepositado)
+        {
+            this._saldo += valorDepositado;
+        }
+
+        public bool Transferir(double valorTransferencia, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valorTransferencia)
+            {
+                return false;
+            }
+            this._saldo -= valorTransferencia;
+            contaDestino.Depositar(valorTransferencia);
+            return true;
+        }
+
+    }
+}

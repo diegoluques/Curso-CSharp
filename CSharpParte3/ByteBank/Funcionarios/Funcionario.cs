c9cb0b21979af086e8bdb960c42e5f53bfb58ea0
Diego Luques <luquesdiego@outlook.com>
2019-11-04 13:39:07 -0400

    Classe Diretor e Sobrecargas

--- CSharpParte3/ByteBank/Funcionarios/Funcionario.cs
+++ CSharpParte3/ByteBank/Funcionarios/Funcionario.cs
@@ -8,26 +8,12 @@ namespace ByteBank.Funcionarios
 {
     public class Funcionario
     {
-        // 0 - Funcionario
-        // 1 - Diretor
-        // 2 - Designer
-        // N - ...
-        private int _tipoFuncionario;
-
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
 
-        public Funcionario(int tipo)
-        {
-            _tipoFuncionario = tipo;
-        }
-
         public double GetBonificacao()
         {
-            if (_tipoFuncionario == 0)
-                return Salario;
-
             return Salario * 0.10;
         }
     }
--- CSharpParte3/ByteBank/Program.cs
+++ CSharpParte3/ByteBank/Program.cs
@@ -11,14 +11,33 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario(0);
-
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            
+            Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "000.000.000-00";
             carlos.Salario = 2000.00;
 
+            gerenciador.Registrar(carlos);
+
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+            
+            Console.WriteLine("");
+
+            Diretor diego = new Diretor();
+            diego.Nome = "Diego";
+            diego.CPF = "000.000.000-01";
+            diego.Salario = 5000;
+
+            gerenciador.Registrar(diego);
+
+            Console.WriteLine(diego.Nome);
+            Console.WriteLine(diego.GetBonificacao());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Total de bonificações: R$ " + gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }

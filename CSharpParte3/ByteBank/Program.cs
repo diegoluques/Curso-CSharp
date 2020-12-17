6feb3bdaac73d2f4f86a4088828e15c95c972fdc
Diego Luques <luquesdiego@outlook.com>
2020-12-17 08:46:48 -0400

    Espaçamentos

--- CSharpParte3/ByteBank/Program.cs
+++ CSharpParte3/ByteBank/Program.cs
@@ -1,52 +1,48 @@
 ﻿using ByteBank.Funcionarios;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
-            
-            Funcionario carlos = new Funcionario();
-            carlos.Nome = "Carlos";
-            carlos.CPF = "000.000.000-00";
-            carlos.Salario = 2000.00;
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
-            gerenciador.Registrar(carlos);
+			Funcionario carlos = new Funcionario();
+			carlos.Nome = "Carlos";
+			carlos.CPF = "000.000.000-00";
+			carlos.Salario = 2000.00;
 
-            Console.WriteLine(carlos.Nome);
-            Console.WriteLine(carlos.GetBonificacao());
-            
-            Console.WriteLine("");
+			gerenciador.Registrar(carlos);
 
-            Diretor roberta = new Diretor();
-            roberta.Nome = "Diego";
-            roberta.CPF = "000.000.000-01";
-            roberta.Salario = 5000;
+			Console.WriteLine(carlos.Nome);
+			Console.WriteLine(carlos.GetBonificacao());
 
-            Funcionario robertaTeste = roberta;
+			Console.WriteLine("");
 
-            Console.WriteLine("Bonificação de uma referência de Diretor: " + roberta.GetBonificacao());
-            Console.WriteLine("Bonificação de uma referência de Funcionário: " + robertaTeste.GetBonificacao());
+			Diretor roberta = new Diretor();
+			roberta.Nome = "Diego";
+			roberta.CPF = "000.000.000-01";
+			roberta.Salario = 5000;
 
-            Console.WriteLine("");
+			Funcionario robertaTeste = roberta;
 
-            gerenciador.Registrar(roberta);
+			Console.WriteLine("Bonificação de uma referência de Diretor: " + roberta.GetBonificacao());
+			Console.WriteLine("Bonificação de uma referência de Funcionário: " + robertaTeste.GetBonificacao());
 
-            Console.WriteLine(roberta.Nome);
-            Console.WriteLine(roberta.GetBonificacao());
+			Console.WriteLine("");
 
-            Console.WriteLine("");
+			gerenciador.Registrar(roberta);
 
-            Console.WriteLine("Total de bonificações: R$ " + gerenciador.GetTotalBonificacao());
+			Console.WriteLine(roberta.Nome);
+			Console.WriteLine(roberta.GetBonificacao());
 
-            Console.ReadLine();
-        }
-    }
+			Console.WriteLine("");
+
+			Console.WriteLine("Total de bonificações: R$ " + gerenciador.GetTotalBonificacao());
+
+			Console.ReadLine();
+		}
+	}
 }

fb606963c776462d95b3b18fad2709f46b83402c
Diego Luques <luquesdiego@outlook.com>
2020-12-17 08:53:13 -0400

    Identação e remoção de using não utilzados

--- CSharpParte3/ByteBank/Funcionarios/Diretor.cs
+++ CSharpParte3/ByteBank/Funcionarios/Diretor.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ByteBank.Funcionarios
+﻿namespace ByteBank.Funcionarios
 {
-    public class Diretor : Funcionario
-    {
-        public override double GetBonificacao()
-        {
-            return Salario + base.GetBonificacao();
-        }
-    }
+	public class Diretor : Funcionario
+	{
+		public override double GetBonificacao()
+		{
+			return Salario + base.GetBonificacao();
+		}
+	}
 }
--- CSharpParte3/ByteBank/GerenciadorBonificacao.cs
+++ CSharpParte3/ByteBank/GerenciadorBonificacao.cs
@@ -1,24 +1,19 @@
 ﻿using ByteBank.Funcionarios;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    public class GerenciadorBonificacao
-    {
-        private double _totalBonificacao;
+	public class GerenciadorBonificacao
+	{
+		private double _totalBonificacao;
 
-        public void Registrar(Funcionario funcionario)
-        {
-            _totalBonificacao += funcionario.GetBonificacao();
-        }
+		public void Registrar(Funcionario funcionario)
+		{
+			_totalBonificacao += funcionario.GetBonificacao();
+		}
 
-        public double GetTotalBonificacao()
-        {
-            return _totalBonificacao;
-        }
-    }
+		public double GetTotalBonificacao()
+		{
+			return _totalBonificacao;
+		}
+	}
 }

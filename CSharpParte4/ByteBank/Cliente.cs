369bbbf6a14d16c77d24b2ba4e25f90affcdeff7
Diego Luques <luquesdiego@outlook.com>
2021-02-21 00:15:48 -0400

    Projeto inicial (exceptions)

---
+++ CSharpParte4/ByteBank/Cliente.cs
@@ -0,0 +1,22 @@
+﻿namespace ByteBank
+{
+	public class Cliente
+	{
+		private string _cpf;
+
+		public string Nome { get; set; }
+
+		public string CPF
+		{
+			get
+			{
+				return _cpf;
+			}
+			set
+			{
+				_cpf = value;
+			}
+		}
+		public string Profissao { get; set; }
+	}
+}

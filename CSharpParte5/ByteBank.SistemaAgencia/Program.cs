f011d8e202c636c7c933eba938a37aad93e8988f
Diego Luques <luquesdiego@outlook.com>
2021-02-28 09:22:01 -0400

    Criação do projeto 5 e projeto de bibliotecas

---
+++ CSharpParte5/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,13 @@
+﻿using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ContaCorrente contaCorrente = new ContaCorrente(2525, 25565);
+
+		}
+	}
+}

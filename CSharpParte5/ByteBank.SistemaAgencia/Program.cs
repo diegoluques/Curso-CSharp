0a9a4aeaf1e2c3867c3950ff3a9ffe88693fc29b
Diego Luques <luquesdiego@outlook.com>
2021-03-01 23:47:17 -0400

    Usando o Humanizer

--- CSharpParte5/ByteBank.SistemaAgencia/Program.cs
+++ CSharpParte5/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,4 @@
-﻿using ByteBank.Modelos;
+﻿using Humanizer;
 using System;
 
 namespace ByteBank.SistemaAgencia
@@ -7,33 +7,21 @@ namespace ByteBank.SistemaAgencia
 	{
 		static void Main(string[] args)
 		{
-			DateTime dataFimPagamento = new DateTime(2021, 4, 17);
+			DateTime dataFimPagamento = new DateTime(2021, 3, 3);
 			DateTime dataCorrente = DateTime.Now;
 
 			TimeSpan diferenca = dataFimPagamento - dataCorrente;
 
-			String msg = "Vencimento em " + RetornarIntervaloDeDatas(diferenca);
+			String msg = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
 			Console.WriteLine("Hoje é: " + dataCorrente);
 			Console.WriteLine("Pagamento em: " + dataFimPagamento);
 			Console.WriteLine(msg);
 
+
 			Console.WriteLine("");
 			Console.ReadLine();
 		}
 
-		private static string RetornarIntervaloDeDatas(TimeSpan timeSpan)
-		{
-			if(timeSpan.Days > 30)
-			{
-				int quantidadeDeMeses = timeSpan.Days / 30;
-				if (quantidadeDeMeses == 1) 
-					return quantidadeDeMeses + " mês";
-
-				return quantidadeDeMeses + " meses";
-			}
-
-			return timeSpan.Days + " dias";
-		}
 	}
 }

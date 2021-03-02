5cbd19879e1a45f550814d2643d517cca5b94c2b
Diego Luques <luquesdiego@outlook.com>
2021-03-01 23:19:00 -0400

    Lidando com datas

--- CSharpParte5/ByteBank.SistemaAgencia/Program.cs
+++ CSharpParte5/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.Modelos;
+using System;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -6,7 +7,33 @@ namespace ByteBank.SistemaAgencia
 	{
 		static void Main(string[] args)
 		{
-			ContaCorrente contaCorrente = new ContaCorrente(2525, 25565);
+			DateTime dataFimPagamento = new DateTime(2021, 4, 17);
+			DateTime dataCorrente = DateTime.Now;
+
+			TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+			String msg = "Vencimento em " + RetornarIntervaloDeDatas(diferenca);
+
+			Console.WriteLine("Hoje é: " + dataCorrente);
+			Console.WriteLine("Pagamento em: " + dataFimPagamento);
+			Console.WriteLine(msg);
+
+			Console.WriteLine("");
+			Console.ReadLine();
+		}
+
+		private static string RetornarIntervaloDeDatas(TimeSpan timeSpan)
+		{
+			if(timeSpan.Days > 30)
+			{
+				int quantidadeDeMeses = timeSpan.Days / 30;
+				if (quantidadeDeMeses == 1) 
+					return quantidadeDeMeses + " mês";
+
+				return quantidadeDeMeses + " meses";
+			}
+
+			return timeSpan.Days + " dias";
 		}
 	}
 }

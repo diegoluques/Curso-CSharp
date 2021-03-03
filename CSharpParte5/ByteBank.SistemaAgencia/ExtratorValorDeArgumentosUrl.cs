58c82aecc198bfcfb910b0f4951fa146eb167d7b
Diego Luques <luquesdiego@outlook.com>
2021-03-02 23:09:08 -0400

    Usando o método Remove

---
+++ CSharpParte5/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosUrl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ByteBank.SistemaAgencia
+{
+	public class ExtratorValorDeArgumentosUrl
+	{
+		private readonly string _argumentos;
+		public string Url { get; }
+
+		public ExtratorValorDeArgumentosUrl(string url)
+		{
+			if (string.IsNullOrEmpty(url))
+				throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
+
+			int indiceInterrogacao = url.IndexOf('?');
+			_argumentos = url.Substring(indiceInterrogacao + 1);
+
+			Url = url;
+		}
+
+		public string ObterValoresDosAgumentos(string nomeDoParametro)
+		{
+			string termo = nomeDoParametro + "=";
+			int indiceParametro = _argumentos.IndexOf(termo);
+
+			string resultado = _argumentos.Substring(indiceParametro + termo.Length);
+			int indiceEcomercial = resultado.IndexOf('&');
+
+			if (indiceEcomercial == -1)
+				return resultado;
+
+			return resultado.Remove(indiceEcomercial);
+		}
+	}
+}
--- CSharpParte5/ByteBank.SistemaAgencia/Program.cs
+++ CSharpParte5/ByteBank.SistemaAgencia/Program.cs
@@ -12,16 +12,47 @@ namespace ByteBank.SistemaAgencia
 
 			TimeSpan diferenca = dataFimPagamento - dataCorrente;
 
-			String msg = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+			//String msg = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
-			Console.WriteLine("Hoje é: " + dataCorrente);
-			Console.WriteLine("Pagamento em: " + dataFimPagamento);
-			Console.WriteLine(msg);
+			//Console.WriteLine("Hoje é: " + dataCorrente);
+			//Console.WriteLine("Pagamento em: " + dataFimPagamento);
+			//Console.WriteLine(msg);
 
+			//TrocarValoresDeDuasVariaveis(5, 7);
+			//LerUmNumeroInteiroERetornarNotasPossiveis(576);
+
+			string urlParametros = "https:www.bytebank.com.br/cambio?moedaOrigem=real&moedaDestino=dolar";
+			ExtratorValorDeArgumentosUrl extratorValorDeArgumentosUrl = new ExtratorValorDeArgumentosUrl(urlParametros);
+
+			string valor2 = extratorValorDeArgumentosUrl.ObterValoresDosAgumentos("moedaOrigem");
+			Console.WriteLine("Valor de moedaOrigem: " + valor2);
+
+			string valor = extratorValorDeArgumentosUrl.ObterValoresDosAgumentos("moedaDestino");
+			Console.WriteLine("Valor de moedaDestino: " + valor);
 
 			Console.WriteLine("");
 			Console.ReadLine();
 		}
 
+		private static void TrocarValoresDeDuasVariaveis(int n, int p)
+		{
+			p = n + p;
+			n = p - n;
+			p -= n;
+
+			Console.WriteLine("n = " + n + "| p = " + p);
+		}
+
+		private static void LerUmNumeroInteiroERetornarNotasPossiveis(int numero)
+		{
+			Console.WriteLine(numero);
+			Console.WriteLine((numero / 100) + " nota(s) de R$ 100, 00");
+			Console.WriteLine((numero / 50) + " nota(s) de R$ 50, 00");
+			Console.WriteLine((numero / 20) + " nota(s) de R$ 20, 00");
+			Console.WriteLine((numero / 10) + " nota(s) de R$ 10, 00");
+			Console.WriteLine((numero / 5) + " nota(s) de R$ 5 ,00");
+			Console.WriteLine((numero / 2) + " nota(s) de R$ 2, 00");
+			Console.WriteLine((numero / 1) + " nota(s) de R$ 1, 00");
+		}
 	}
 }

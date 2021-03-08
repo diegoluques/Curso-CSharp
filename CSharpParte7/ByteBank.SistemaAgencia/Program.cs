a65f28cf4977d92cdb4652bb2cfc5058a44ebf07
Diego Luques <luquesdiego@outlook.com>
2021-03-07 22:46:47 -0400

    Conhecendo array

--- CSharpParte7/ByteBank.SistemaAgencia/Program.cs
+++ CSharpParte7/ByteBank.SistemaAgencia/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ByteBank.Modelos;
-using ByteBank.Modelos.Funcionarios;
 
 namespace ByteBank.SistemaAgencia
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadLine();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] idades = new int[5];
+
+			idades[0] = 15;
+			idades[1] = 25;
+			idades[2] = 35;
+			idades[3] = 50;
+			idades[4] = 28;
+
+
+			Console.WriteLine(idades[4]);
+
+			Console.ReadLine();
+		}
+	}
 }

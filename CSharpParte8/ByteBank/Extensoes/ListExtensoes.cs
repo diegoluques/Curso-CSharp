54fb93525dc1745a2095ec8193fd91202c1d757c
Diego Luques <luquesdiego@outlook.com>
2021-03-15 22:14:18 -0400

    Método de extensão em classe genérica

---
+++ CSharpParte8/ByteBank/Extensoes/ListExtensoes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ByteBank.Extensoes
+{
+	public static class ListExtensoes
+	{
+		public static void AdicionarVarios<T>(this List<T> lista, params T[] itens)
+		{
+			foreach (T item in itens)
+			{
+				lista.Add(item);
+			}
+		}
+	}
+}
--- CSharpParte8/ByteBank/Program.cs
+++ CSharpParte8/ByteBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Extensoes;
+using System;
 using System.Collections.Generic;
 
 namespace ByteBank
@@ -7,27 +8,38 @@ namespace ByteBank
 	{
 		static void Main(string[] args)
 		{
+			ListaDeInteiros();
+			Console.WriteLine("");
+			ListaDeStrings();
+
+			Console.ReadLine();
+		}
+
+		private static void ListaDeInteiros()
+		{
 			List<int> idades = new List<int>();
 
-			//ListExtensoes.AdicionarVarios(idades, 1, 5, 14, 25, 38, 61);
+			ListExtensoes.AdicionarVarios(idades, 1, 5, 14, 25, 38, 61);
 
 			idades.AdicionarVarios(1, 5, 14, 25, 38, 61);
-
-			idades.Add(1);
-			idades.Add(5);
-			idades.Add(14);
-			idades.Add(25);
-			idades.Add(38);
-			idades.Add(61);
-
 			idades.Remove(5);
 
 			for (int i = 0; i < idades.Count; i++)
 			{
 				Console.WriteLine(idades[i]);
 			}
+		}
 
-			Console.ReadLine();
+		private static void ListaDeStrings()
+		{
+			List<string> idades = new List<string>();
+
+			ListExtensoes.AdicionarVarios(idades, "A", "B", "C", "D", "E");
+
+			for (int i = 0; i < idades.Count; i++)
+			{
+				Console.WriteLine(idades[i]);
+			}
 		}
 	}
 }

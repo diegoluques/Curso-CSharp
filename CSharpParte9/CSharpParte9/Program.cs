91ba7364bde2d4df654885d1b3af70e3b0a0fc9d
Diego Luques <luquesdiego@outlook.com>
2021-03-17 07:40:19 -0400

    Criando um FileStream

--- CSharpParte9/CSharpParte9/Program.cs
+++ CSharpParte9/CSharpParte9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CSharpParte9
 {
@@ -6,7 +7,30 @@ namespace CSharpParte9
 	{
 		static void Main(string[] args)
 		{
+			var enderecoDoArquivo = "contas.txt";
+
+			var fluxoDoArquivo = new FileStream(path: enderecoDoArquivo, mode: FileMode.Open);
+
+			var buffer = new byte[1024]; //1kb
+			var numeroDeBytesLidos = -1;
+
+			while (numeroDeBytesLidos != 0)
+			{
+				numeroDeBytesLidos = fluxoDoArquivo.Read(array: buffer, offset: 0, count: 1024);
+				EscreverBuffer(buffer);
+			}
+
+			Console.Write("");
 			Console.ReadLine();
+		}
+
+		static void EscreverBuffer(byte[] buffer)
+		{
+			foreach (var meuByte in buffer)
+			{
+				Console.Write(meuByte);
+				Console.Write("-");
+			}
 		}
 	}
 }

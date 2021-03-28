9716f0bbe3fe9fcd461fc6d006280b7d919678dc
Diego Luques <luquesdiego@outlook.com>
2021-03-28 12:44:35 -0400

    Método Close e using

--- CSharpParte9/CSharpParte9/Program.cs
+++ CSharpParte9/CSharpParte9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace CSharpParte9
 {
@@ -9,15 +10,15 @@ namespace CSharpParte9
 		{
 			var enderecoDoArquivo = "contas.txt";
 
-			var fluxoDoArquivo = new FileStream(path: enderecoDoArquivo, mode: FileMode.Open);
-
-			var buffer = new byte[1024]; //1kb
-			var numeroDeBytesLidos = -1;
-
-			while (numeroDeBytesLidos != 0)
+			using (var fluxoDoArquivo = new FileStream(path: enderecoDoArquivo, mode: FileMode.Open))
 			{
-				numeroDeBytesLidos = fluxoDoArquivo.Read(array: buffer, offset: 0, count: 1024);
-				EscreverBuffer(buffer);
+				var buffer = new byte[1024]; //1kb
+				var numeroDeBytesLidos = -1;
+				while (numeroDeBytesLidos != 0)
+				{
+					numeroDeBytesLidos = fluxoDoArquivo.Read(array: buffer, offset: 0, count: 1024);
+					EscreverBuffer(buffer);
+				}
 			}
 
 			Console.Write("");
@@ -26,11 +27,15 @@ namespace CSharpParte9
 
 		static void EscreverBuffer(byte[] buffer)
 		{
-			foreach (var meuByte in buffer)
-			{
-				Console.Write(meuByte);
-				Console.Write("-");
-			}
+			var utf8 = Encoding.Default;
+			var texto = utf8.GetString(buffer);
+			Console.WriteLine(texto);
+
+			//foreach (var meuByte in buffer)
+			//{
+			//	Console.Write(meuByte);
+			//	Console.Write("-");
+			//}
 		}
 	}
 }

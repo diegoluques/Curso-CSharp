ae73de555fe827ebea9ba44a6f11a67cd2463014
Diego Luques <luquesdiego@outlook.com>
2021-04-04 16:55:30 -0400

    Criando arquivos CSV

---
+++ CSharpParte9/CSharpParte9/CriandoArquivoCsv.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+
+namespace CSharpParte9
+{
+	partial class Program
+	{
+		static void CriarArquivo()
+		{
+			var caminhoNovoArquivo = "contasExportadas.csv";
+
+			using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+			{
+				var contaComoString = "00001;0001;1999.99;Diego Luques";
+				var encoding = Encoding.UTF8;
+
+				var bytes = encoding.GetBytes(contaComoString);
+
+				fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+			}
+		}
+	}
+}
--- CSharpParte9/CSharpParte9/Program.cs
+++ CSharpParte9/CSharpParte9/Program.cs
@@ -1,6 +1,4 @@
-﻿using CSharpParte9.Modelos;
-using System;
-using System.IO;
+﻿using System;
 
 namespace CSharpParte9
 {
@@ -8,43 +6,10 @@ namespace CSharpParte9
 	{
 		static void Main(string[] args)
 		{
-			//LidandoComStreamDiretamente();
+			CriarArquivo();
 
-			var enderecoDoArquivo = "contas.txt";
-			using (var fluxoDoArquivo = new FileStream(path: enderecoDoArquivo, mode: FileMode.Open))
-			using (var leitor = new StreamReader(fluxoDoArquivo))
-			{
-				while (!leitor.EndOfStream)
-				{
-					var linha = leitor.ReadLine();
-
-					var contaCorrente = ConverterStringParaContaCorrente(linha);
-					var msg = $"Titutal: {contaCorrente.Titular.Nome}, Conta número: {contaCorrente.Numero}, Ag: {contaCorrente.Agencia}, Saldo: {contaCorrente.Saldo}";
-					Console.WriteLine(msg);
-					//Console.WriteLine(linha);
-				}
-			}
 			Console.Write("");
 			Console.ReadLine();
-		}
-
-		static ContaCorrente ConverterStringParaContaCorrente(string linha)
-		{
-			string[] campos = linha.Split(';');
-
-			var agencia = campos[0];
-			var numero = campos[1];
-			var saldo = campos[2].Replace(".", ",");
-			var nomeTitular = campos[3];
-
-			var titular = new Cliente();
-			titular.Nome = nomeTitular;
-
-			var resultado = new ContaCorrente(int.Parse(agencia), int.Parse(numero));
-			resultado.Depositar(double.Parse(saldo));
-			resultado.Titular = titular;
-
-			return resultado;
 		}
 	}
 }
